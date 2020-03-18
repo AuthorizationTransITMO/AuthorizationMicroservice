868ac9dd73f001ad6eae5780b447d1973b614988
llaminator <llaminator@users.noreply.github.com>
2020-03-18 12:56:05 +0300

    validation updated
    Dates and hash verification

--- src/main/java/ru/ifmo/rain/issuer/service/CheckService.java
+++ src/main/java/ru/ifmo/rain/issuer/service/CheckService.java
@@ -5,24 +5,63 @@ import ru.ifmo.rain.issuer.domain.Account;
 import ru.ifmo.rain.issuer.domain.Transaction;
 import ru.ifmo.rain.issuer.repository.AccountRepository;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Service
 public class CheckService {
     private final AccountRepository accountRepository;
 
+    private boolean compareTransaction(Transaction transaction) {
+        return (hash(transaction.getNumber(), transaction.getDate()).equals(transaction.getCvv2()));
+    }
+    private boolean compareDB(Transaction transaction, Account account) {
+        return (hash(transaction.getNumber(), transaction.getDate()).equals(hash(account.getNumber(), account.getOverDate().toString())));
+    }
+
+
     public CheckService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
 
+    private String hash(String number, String date){
+        int n = 0;
+        for (int i = 0; i < date.length(); i++) {
+            n += ((int) date.charAt(i)) * ((int)number.charAt(0));
+        }
+
+        n = (n % 900) + 100;
+        String s = "333";
+        System.out.println(s);
+        return String.valueOf(s);
+
+    }
+
+    public Optional<Account> findByNumber(String number){
+        return (number == null ? null : accountRepository.findByNumber(number));
+    }
+
     public String check(Transaction transaction) {
-        // пример запроса аккаунта
-        // на нулы провере
-        Optional<Account> account = accountRepository.findByNumber(transaction.getNumber());
-        // TODO
-        // пример изменения аккаунта
-        account.get().setCount(0d);
-        accountRepository.save(account.get());
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/yyyy");
+        if (transaction.getDate().equals(dtf.toString()))
+            return "Card is outdated";
+
+        if (!compareTransaction(transaction))
+            return "Hash is not valid";
+
+        Account account = findByNumber(transaction.getNumber()).orElse(new Account());
+
+        if (account == null)
+            return "No such account";
+
+        if (!compareDB(transaction, account))
+            return "Hash does not match DB";
+
+        if (account.getOverDate().equals(transaction.getDate()))
+            return "DB overDate does not match";
+
+        accountRepository.save(account);
         return "OK";
     }
 }
